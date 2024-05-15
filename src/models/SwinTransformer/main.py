d59939ef8aa6370ad146a899ca388d71977fcf50
andreleo02 <andreleo02@gmail.com>
2024-05-15 12:07:43 +0200

    added device control for gpu

--- src/models/SwinTransformer/main.py
+++ src/models/SwinTransformer/main.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision.models import SwinTransformer, swin_t, Swin_T_Weights
+from torch import DeviceObjType
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 from torchvision.datasets import Flowers102
@@ -16,11 +17,12 @@ def load_model() -> SwinTransformer:
 
 def optimal_model(model: SwinTransformer,
                  dataset_name: str,
-                 num_epochs: int = 10,
-                 learning_rate: float = 0.001,
-                 num_classes: int = 0,
-                 batch_size: int = 32,
-                 patience: int = 3)-> SwinTransformer:
+                 num_epochs: int,
+                 learning_rate: float,
+                 num_classes: int,
+                 batch_size: int,
+                 patience: int,
+                 device: DeviceObjType)-> SwinTransformer:
     model.head = nn.Linear(model.head.in_features, num_classes)
 
     transform = transforms.Compose([
@@ -38,7 +40,7 @@ def optimal_model(model: SwinTransformer,
     train_loader = DataLoader(train_dataset, batch_size = batch_size, shuffle = True)
     val_loader = DataLoader(val_dataset, batch_size = batch_size, shuffle = False)
 
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss().to(device = device)
     optimizer = optim.Adam(model.parameters(), lr = learning_rate)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size = 3, gamma = 0.1)
 
@@ -46,10 +48,11 @@ def optimal_model(model: SwinTransformer,
     epochs_no_improve = 0
     for epoch in range(num_epochs):
         
-        train_loss, train_acc = train_model(model = model, train_loader = train_loader, optimizer = optimizer, criterion = criterion)
+        train_loss, train_acc = train_model(model = model, train_loader = train_loader, optimizer = optimizer, criterion = criterion, device = device)
         scheduler.step()
-        val_loss, val_acc = validate_model(model = model, val_loader = val_loader, criterion = criterion)
+        val_loss, val_acc = validate_model(model = model, val_loader = val_loader, criterion = criterion, device = device)
         print(f"Epoch {epoch}, Train loss: {train_loss}, Validation loss: {val_loss}")
+        print(f"Epoch {epoch}, Train accuracy: {train_acc}, Validation accuracy: {val_acc}")
 
         wandb.log({
             # log training stats
@@ -71,13 +74,14 @@ def optimal_model(model: SwinTransformer,
 
     return model
 
-def train_model(model: SwinTransformer, train_loader: DataLoader, optimizer, criterion):
+def train_model(model: SwinTransformer, train_loader: DataLoader, optimizer, criterion, device):
     train_loss = 0
     train_acc = 0
-    for inputs, labels in train_loader:
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
         model.train()
         optimizer.zero_grad()
-        outputs = model(inputs)
+        outputs = model(images).to(device)
         loss = criterion(outputs, labels)
         loss.backward()
         train_loss += loss.item()
@@ -87,13 +91,14 @@ def train_model(model: SwinTransformer, train_loader: DataLoader, optimizer, cri
     training_accuracy = train_acc / len(train_loader)
     return training_loss, training_accuracy
 
-def validate_model(model: SwinTransformer, val_loader: DataLoader, criterion):
+def validate_model(model: SwinTransformer, val_loader: DataLoader, criterion, device):
     model.eval()
     val_loss = 0
     val_accuracy = 0
     with torch.no_grad():
         for images, labels in val_loader:
-            outputs = model(images)
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images).to(device)
             loss = criterion(outputs, labels)
             val_loss += loss.item()
             val_accuracy += calc_accuracy(labels, outputs.argmax(dim = 1))
@@ -152,5 +157,6 @@ if __name__ == "__main__":
                          learning_rate = learning_rate,
                          num_classes = 102,
                          batch_size = batch_size,
-                         patience = patience)
+                         patience = patience,
+                         device = device)
 
