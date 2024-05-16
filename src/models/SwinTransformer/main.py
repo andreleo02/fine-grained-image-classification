452ad9480bd9734b6e8e29a768a4d22c762552e4
andreleo02 <andreleo02@gmail.com>
2024-05-16 10:36:16 +0200

    refactor

--- src/models/SwinTransformer/main.py
+++ src/models/SwinTransformer/main.py
@@ -1,130 +1,19 @@
-import yaml
-import argparse
-import wandb
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torchvision.models import SwinTransformer, swin_t, Swin_T_Weights
-from torch import DeviceObjType
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+import argparse, sys, os
+from torchvision.models import swin_b
 from torchvision.datasets import Flowers102
-from pathlib import Path
 
-from utils import train_model, validate_model, test_model, get_data
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..')))
 
-def load_model() -> SwinTransformer:
-    # weights = Swin_T_Weights.DEFAULT
-    return swin_t(weights = 0)
-
-def optimal_model(model: SwinTransformer,
-                 dataset_name: str,
-                 num_epochs: int,
-                 learning_rate: float,
-                 num_classes: int,
-                 batch_size: int,
-                 patience: int,
-                 device: DeviceObjType)-> SwinTransformer:
-    model.head = nn.Linear(model.head.in_features, num_classes)
-
-    transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225]),
-    ])
-
-    train_loader, val_loader, test_loader = get_data(dataset_name, batch_size, transform)
-
-    criterion = nn.CrossEntropyLoss().to(device = device)
-    optimizer = optim.Adam(model.parameters(), lr = learning_rate)
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size = 3, gamma = 0.1)
-
-    best_val_loss = float('inf')
-    epochs_no_improve = 0
-    for epoch in range(num_epochs):
-        
-        train_loss, train_acc = train_model(model = model, train_loader = train_loader, optimizer = optimizer, criterion = criterion, device = device)
-        scheduler.step()
-        val_loss, val_acc = validate_model(model = model, val_loader = val_loader, criterion = criterion, device = device)
-        print(f"Epoch {epoch}, Train loss: {train_loss}, Validation loss: {val_loss}")
-        print(f"Epoch {epoch}, Train accuracy: {train_acc}, Validation accuracy: {val_acc}")
-
-        wandb.log({
-            # log training stats
-            "train/loss":train_loss,
-            "train/accuracy":train_acc,
-            # log validation stats
-            "val/loss":val_loss,
-            "val/accuracy":val_acc
-        })
-
-        # Save the model checkpoints
-        # if e % save_every_n_epochs or e == (epochs - 1):
-        #     torch.save(net.state_dict(), checkpoint_path / f'epoch-{e}.pth')
-
-        # # Update the best model so far
-        # if val_accuracy >= best_val_accuracy:
-        #     torch.save(net.state_dict(), checkpoint_path / f'best.pth')
-        #     best_val_accuracy = val_accuracy
-
-        if val_loss < best_val_loss:
-            best_val_loss = val_loss
-            epochs_no_improve = 0
-        else:
-            epochs_no_improve += 1
-            if epochs_no_improve == patience:
-                print(f"Early stop at epoch {epoch}.")
-                break
-
-    return model
+from src.utils import main
 
 if __name__ == "__main__":
-
-    wandb.login()
-
-    torch.manual_seed(1234)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     parser = argparse.ArgumentParser("")
     parser.add_argument("--config", required = True, type = str, help = "Path to the configuration file")
     parser.add_argument("--run_name", required = False, type = str, help = "Name of the run")
     args = parser.parse_args()
-    with open(args.config, "r") as f:
-        config = yaml.safe_load(f)
-
-    learning_rate = config["training"]["lr"]
-    num_epochs = config["training"]["num_epochs"]
-    run_name = config["run_name"]
-    batch_size = config["data"]["batch_size"]
-    patience = config["training"]["patience"]
-
-    # WandB initialization
-    wandb.init(
-        # Set the project where this run will be logged
-        project="Competition",
-        # Set a run name
-        # (otherwise it’ll be randomly assigned, like sunshine-lollypop-10)
-        name=f"{run_name}",
-        # Track hyperparameters and run metadata
-        config={
-        "learning_rate": learning_rate,
-        "dataset": "Flowers102",
-        "epochs": num_epochs,
-        "batch_size":batch_size,
-        })
-
-    checkpoint_path = Path("./checkpoint")
-    checkpoint_path = checkpoint_path / run_name
-    checkpoint_path.mkdir(exist_ok = True, parents = True)
-
-    model = load_model()
-    model = optimal_model(model = model,
-                         dataset_name = "Flowers102", 
-                         num_epochs = num_epochs,
-                         learning_rate = learning_rate,
-                         num_classes = 102,
-                         batch_size = batch_size,
-                         patience = patience,
-                         device = device)
-
+    main(args = args,
+         model_function = swin_b,
+         dataset_function = Flowers102,
+         dataset_name = "Flowers102",
+         num_classes = 102)
--- src/utils.py
+++ src/utils.py
@@ -1,29 +1,72 @@
-from kaggle.api.kaggle_api_extended import KaggleApi
-import os, requests, zipfile, tarfile
-import yaml
-import argparse
+import os, requests, zipfile, tarfile, yaml
 import wandb
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torchvision.models import SwinTransformer, swin_t, Swin_T_Weights
-from torch import DeviceObjType
-from torchvision import datasets, transforms
+from kaggle.api.kaggle_api_extended import KaggleApi
 from torch.utils.data import DataLoader
-from torchvision.datasets import Flowers102
+from torchvision import transforms
 from pathlib import Path
+from torch import DeviceObjType
+from torchvision import transforms
+from torch.utils.data import DataLoader
 
-def get_data(dataset_name, batch_size, transform):
-    dataset_path = "../../data/" + dataset_name
-    train_dataset = Flowers102(root = dataset_path, split = "train", transform = transform, download = True)
-    val_dataset = Flowers102(root = dataset_path, split = "val", transform = transform, download = True)
-    test_dataset = Flowers102(root = dataset_path, split = "test", transform = transform, download = True)
 
-    train_loader = DataLoader(train_dataset, batch_size = batch_size, shuffle = True)
-    val_loader = DataLoader(val_dataset, batch_size = batch_size, shuffle = False)
-    test_loader = DataLoader(test_dataset, batch_size = batch_size, shuffle = False)
+def load_model(function, weights = None):
+    return function(weights = weights)
 
-    return train_loader, val_loader, test_loader
+def optimal_model(model,
+                  train_loader: DataLoader,
+                  val_loader: DataLoader,
+                  num_epochs: int,
+                  learning_rate: float,
+                  num_classes: int,
+                  patience: int,
+                  device: DeviceObjType,
+                  net: str,
+                  checkpoint_path):
+    model.head = nn.Linear(model.head.in_features, num_classes)
+
+    criterion = nn.CrossEntropyLoss().to(device = device)
+    optimizer = optim.Adam(model.parameters(), lr = learning_rate)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size = 3, gamma = 0.1)
+
+    best_val_loss = float('inf')
+    epochs_no_improve = 0
+    for epoch in range(num_epochs):
+        train_loss, train_acc = train_model(model = model,
+                                            train_loader = train_loader,
+                                            optimizer = optimizer,
+                                            criterion = criterion,
+                                            device = device)
+        scheduler.step()
+        val_loss, val_acc = validate_model(model = model,
+                                           val_loader = val_loader,
+                                           criterion = criterion,
+                                           device = device)
+        print(f"Epoch {epoch}, Train loss: {train_loss}, Validation loss: {val_loss}")
+        print(f"Epoch {epoch}, Train accuracy: {train_acc}, Validation accuracy: {val_acc}")
+
+        wandb.log({
+            "train/loss":train_loss,
+            "train/accuracy":train_acc,
+            "val/loss":val_loss,
+            "val/accuracy":val_acc
+        })
+
+        torch.save(net.state_dict(), checkpoint_path / f'epoch-{epoch}.pth')
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            torch.save(net.state_dict(), checkpoint_path / f'best.pth')
+            epochs_no_improve = 0
+        else:
+            epochs_no_improve += 1
+            if epochs_no_improve == patience:
+                print(f"Validation Accuracy did not imporove for {patience} epochs. Killing the training...")
+                break
+
+    return model
 
 def train_model(model, train_loader: DataLoader, optimizer, criterion, device):
     model.train()
@@ -60,21 +103,84 @@ def validate_model(model, val_loader: DataLoader, criterion, device):
     val_accuracy /= len(val_loader)
     return val_loss, val_accuracy
 
-def test_model(model, val_loader: DataLoader, criterion, device):
+def test_model(model, test_loader: DataLoader, device):
     model.eval()
     model.to(device)
-    val_loss = 0
-    val_accuracy = 0
+    test_accuracy = 0
     with torch.no_grad():
-        for images, labels in val_loader:
+        for images, labels in test_loader:
             images, labels = images.to(device), labels.to(device)
             outputs = model(images)
-            loss = criterion(outputs, labels)
-            val_loss += loss.item()
-            val_accuracy += calc_accuracy(labels, outputs.argmax(dim = 1))
-    val_loss /= len(val_loader)
-    val_accuracy /= len(val_loader)
-    return val_loss, val_accuracy
+            test_accuracy += calc_accuracy(labels, outputs.argmax(dim = 1))
+    test_accuracy /= len(test_loader)
+
+    print(f"Test accuracy: {test_accuracy}")
+
+def main(args, model_function, dataset_function, dataset_name, num_classes):
+    wandb.login()
+
+    torch.manual_seed(1234)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    with open(args.config, "r") as f:
+        config = yaml.safe_load(f)
+
+    net = config["net"]
+    learning_rate = config["training"]["lr"]
+    num_epochs = config["training"]["num_epochs"]
+    patience = config["training"]["patience"]
+    batch_size = config["data"]["batch_size"]
+
+    wandb.init(
+        project = "Competition",
+        name = f"{args.run_name}",
+        config = {
+        "learning_rate": learning_rate,
+        "architecture": net,
+        "dataset": dataset_name,
+        "epochs": num_epochs,
+        "batch_size":batch_size,
+        })
+
+    checkpoint_path = Path("./checkpoint")
+    checkpoint_path = checkpoint_path / args.run_name
+    checkpoint_path.mkdir(exist_ok = True, parents = True)
+
+    model = load_model(model_function)
+
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225]),
+    ])
+    train_loader, val_loader = get_data(dataset_function = dataset_function,
+                                                     dataset_name = dataset_name,
+                                                     batch_size = batch_size,
+                                                     transform = transform)
+    
+    model = optimal_model(model = model,
+                          train_loader = train_loader,
+                          val_loader = val_loader,
+                          num_epochs = num_epochs,
+                          learning_rate = learning_rate,
+                          num_classes = num_classes,
+                          patience = patience,
+                          device = device,
+                          net = net,
+                          checkpoint_path = checkpoint_path)
+    # test_model(model, test_loader, device = device)
+
+def get_data(dataset_function, dataset_name, batch_size, transform):
+    dataset_path = "../../data/" + dataset_name
+    train_dataset = dataset_function(root = dataset_path, split = "train", transform = transform, download = True)
+    val_dataset = dataset_function(root = dataset_path, split = "val", transform = transform, download = True)
+    # test_dataset = dataset_function(root = dataset_path, split = "test", transform = transform, download = True)
+
+    train_loader = DataLoader(train_dataset, batch_size = batch_size, shuffle = True)
+    val_loader = DataLoader(val_dataset, batch_size = batch_size, shuffle = False)
+    # test_loader = DataLoader(test_dataset, batch_size = batch_size, shuffle = False)
+
+    return train_loader, val_loader
 
 def calc_accuracy(y_true, y_pred):
     correct = torch.eq(y_true, y_pred).sum().item()
