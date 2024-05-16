1dc671395f383d054c4e57b130ec0ce358fb8e29
andreleo02 <andreleo02@gmail.com>
2024-05-16 10:42:42 +0200

    removed kaggle download

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,6 @@ import wandb
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from kaggle.api.kaggle_api_extended import KaggleApi
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from pathlib import Path
@@ -186,13 +185,6 @@ def calc_accuracy(y_true, y_pred):
     correct = torch.eq(y_true, y_pred).sum().item()
     acc = (correct / len(y_pred)) * 100
     return acc
-
-def download_dataset_from_kaggle(dataset_name: str, output_dir: str = "dataset") -> None:
-    api = KaggleApi()
-    api.authenticate()
-    final_output_dir: str = "src/data/" + output_dir
-    api.dataset_download_files(dataset = dataset_name, path = final_output_dir, unzip = True)
-    print(f"Dataset {dataset_name} downloaded correctly from Kaggle")
 
 def download_dataset_zip(url: str, output_dir: str = "dataset") -> None:
     response = requests.get(url)
