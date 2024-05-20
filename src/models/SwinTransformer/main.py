d8329d6306e8cfc863549830c3ea6513cda926aa
Sonia Borsi <soniaborsi1101@gmail.com>
2024-05-20 10:03:36 +0200

    changed n of categories for cub

--- src/models/SwinTransformer/main.py
+++ src/models/SwinTransformer/main.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    num_classes = 102
+    num_classes = 200
 
     model = load_model(swin_t, weights = Swin_T_Weights.IMAGENET1K_V1)
 
