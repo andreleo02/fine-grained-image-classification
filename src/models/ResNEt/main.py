db5fb7086ef1110a15e77dda63f30a9c69de6358
Sonia Borsi <soniaborsi1101@gmail.com>
2024-05-20 10:08:41 +0200

    changed resnet  main

--- src/models/ResNEt/main.py
+++ src/models/ResNEt/main.py
@@ -2,7 +2,7 @@ import argparse, sys, os, yaml
 import torch.nn as nn
 from torchvision.models import ResNet18_Weights, resnet18
 from torchvision.datasets import FGVCAircraft
-import torch.optim as optim 
+import torch.optim as optim
 import torch
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..')))
@@ -12,14 +12,14 @@ from src.utils import main, load_model, freeze_layers
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser("")
-    parser.add_argument("--config", required = True, type = str, help = "Path to the configuration file")
-    parser.add_argument("--run_name", required = False, type = str, help = "Name of the run")
+    parser.add_argument("--config", required=True, type=str, help="Path to the configuration file")
+    parser.add_argument("--run_name", required=False, type=str, help="Name of the run")
     args = parser.parse_args()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: '{device}'")
 
-    model = load_model(resnet18, weights = ResNet18_Weights.IMAGENET1K_V1)
+    model = load_model(resnet18, weights=ResNet18_Weights.IMAGENET1K_V1)
 
     num_classes = 100
 
@@ -32,22 +32,22 @@ if __name__ == "__main__":
     step_size = config["training"]["scheduler"]["step_size"]
     gamma = config["training"]["scheduler"]["gamma"]
 
-    model.classifier[1] = nn.Linear(model.classifier[1].in_features, num_classes)
-    freeze_layers(model = model, num_blocks_to_freeze = frozen_layers)
-    
+    model.fc = nn.Linear(model.fc.in_features, num_classes)  # Correctly modifying the fc layer
+    freeze_layers(model=model, num_blocks_to_freeze=frozen_layers)
+
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()),
-                          lr = learning_rate,
-                          momentum = momentum,
-                          weight_decay = weight_decay)
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size = step_size, gamma = gamma)
+                          lr=learning_rate,
+                          momentum=momentum,
+                          weight_decay=weight_decay)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
 
-    main(args = args,
-         model = model,
-         dataset_function = FGVCAircraft,
-         dataset_name = "FGVCAircraft",
-         criterion = criterion,
-         optimizer = optimizer,
-         scheduler = scheduler,
-         device = device,
-         config = config)
+    main(args=args,
+         model=model,
+         dataset_function=FGVCAircraft,
+         dataset_name="FGVCAircraft",
+         criterion=criterion,
+         optimizer=optimizer,
+         scheduler=scheduler,
+         device=device,
+         config=config)
